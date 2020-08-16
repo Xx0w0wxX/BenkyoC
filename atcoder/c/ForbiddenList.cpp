a380345181c93d204a7a0b67883bc423fcf3f4b7
Xx0w0wxX <qx0w0xp@protonmail.com>
2020-08-16 18:51:20 +0900

    084

---
+++ atcoder/c/ForbiddenList.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+
+int main()
+{
+    int x;
+    int n;
+    int rcounter;
+    int lcounter;
+    rcounter = 0;
+    lcounter = 0;
+
+    cin >> x >> n;
+    vector<int> v(n);
+
+    rep(it, 0, n)
+        cin >> v[it];
+
+    if (n==0) {
+        cout << x << "\n";
+        return 0;
+    }
+
+    while (true) {
+        if (find(v.begin(), v.end(), x+rcounter) != v.end()) {
+            // in right
+            rcounter++;
+            if (find(v.begin(), v.end(), x-lcounter) != v.end()) {
+                // in left
+                lcounter++;
+                continue;
+            }
+        } else {
+            // not in right
+            if (find(v.begin(), v.end(), x-lcounter) != v.end()) {
+                // in left
+                cout << x+rcounter << "\n";
+                return 0;
+            } else {
+                cout << x-lcounter << "\n";
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
