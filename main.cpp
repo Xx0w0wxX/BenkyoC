aa4eecc0b26dfa15ba43be186dd4d8d27e42ec0b
Xx0w0wxX <qx0w0xp@protonmail.com>
2020-09-02 08:16:20 +0900

    084

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fo(i,a,b) for(int i=(a);i<=(b);++i)
+#define fd(i,b,a) for(int i=(b);i>=(a);--i)
+
+typedef long long ll;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
