98b902f69febc353497571e6a4254ea56b74c5ec
Xx0w0wxX <qx0w0xp@protonmail.com>
2020-09-21 20:03:18 +0900

    084

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 typedef long long ll;
+typedef vector<ll> vl;
+
+#define SQ(a) (a)*(a)
 
 #define fo(i,a,b) for(ll i=(a);i<=(b);++i)
 #define fd(i,b,a) for(ll i=(b);i>=(a);--i)
@@ -12,4 +15,5 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    return 0;
 }
